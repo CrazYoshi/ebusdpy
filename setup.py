120e2f58bd46272029a55773505c3bb5d2e6d04c
CrazYoshi <mar862002@hotmail.com>
2019-11-09 13:35:54 +0100

    change pypi library version to 0.0.16

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ebusdpy",
-    version="0.0.10",
+    version="0.0.16",
     author="CrazYoshi",
     author_email="crazyoshi1186@gmail.com",
     description="eBusd python integration library",
